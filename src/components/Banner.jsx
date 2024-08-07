7d0d03e59007ae73293d191dae828d07826ba53b
Robson <robson.2501@hotmail.com>
2024-08-07 15:41:05 -0300

    fix: typo on banner page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import recipes from '../utils/recipes.json';
+import React, { useEffect, useState } from 'react'
+import recipes from '../utils/recipes.json'
 
 const Banner = () => {
-  const [recipeList, setRecipeList] = useState([]);
+  const [recipeList, setRecipeList] = useState([])
 
   useEffect(() => {
-    setRecipeList([...recipes, ...recipes]);
-  }, []);
+    setRecipeList([...recipes, ...recipes])
+  }, [])
 
   return (
     <div className="relative max-h-40 w-full p-4 flex items-center justify-center overflow-hidden">
@@ -21,11 +21,11 @@ const Banner = () => {
           </div>
         ))}
       </div>
-      <h2 className="absolute bottom-4 text-2xl font-semibold text-white bg-black bg-opacity-50 p-2 rounded">
+      <h2 className="absolute bottom-12 text-2xl font-semibold text-white bg-black bg-opacity-50 p-2 rounded">
         Sua comida a um clique de distância
       </h2>
     </div>
-  );
-};
+  )
+}
 
-export default Banner;
+export default Banner
