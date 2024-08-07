2d4e906faedea0c64c9463a0859c811165f9cc94
Robson <robson.2501@hotmail.com>
2024-08-07 20:27:57 -0300

    feat: install formik and yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import Footer from "../components/Footer"
 
 const Login = React.FC = () => {
     return (
-        <div>
-            Login
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-grow">
+
+            </div>
+            <Footer />
         </div>
     )
 }
