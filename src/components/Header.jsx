10e51b032f8242fe8734f2177c9a61fd364b3ce1
Robson <robson.2501@hotmail.com>
2024-08-09 20:38:32 -0300

    fix: redirect added on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = React.FC = () => {
     return (
         <header className="bg-blue-600 text-white p-4">
           <div className="container mx-auto flex justify-between items-center">
-            <h1 className="text-xl font-bold">Stardew Valley Food</h1>
+            <Link to="/">
+              <h1 className="text-xl font-bold">Stardew Valley Food</h1>
+            </Link>
             <div>
               <Link to="/login">
                 <button 
