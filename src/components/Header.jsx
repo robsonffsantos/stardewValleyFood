d5dbbb9bfe851c5e4ad4aac5520a5a5a888549fd
Robson <robson.2501@hotmail.com>
2024-08-30 01:57:20 -0300

    feat: add farmer photo, edit Header to adapt if user is logged

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,29 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useAuth } from '../context/LoginContext'
 
-const Header = React.FC = () => {
-    return (
-        <header className="bg-blue-600 text-white p-4">
-          <div className="container mx-auto flex justify-between items-center">
-            <Link to="/">
-              <h1 className="text-xl font-bold">Stardew Valley Food</h1>
-            </Link>
-            <div>
+const Header = () => {
+  const { user } = useAuth()
+
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/">
+          <h1 className="text-xl font-bold">Stardew Valley Food</h1>
+        </Link>
+        <div className="flex items-center">
+          {user ? (
+            <>
+              <img 
+                src={user.foto} 
+                alt={`${user.name} profile`} 
+                className="w-8 h-8 rounded-full mr-2" 
+              />
+              <span className="mr-4">{user.name}</span>
+              <span className="font-semibold">{`Saldo: R$ ${user.saldo.toFixed(2)}`}</span>
+            </>
+          ) : (
+            <>
               <Link to="/login">
                 <button 
                   className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200"
@@ -16,14 +31,16 @@ const Header = React.FC = () => {
                   Login
                 </button>
               </Link>
-              <a className="mx-2 text-xs">ou</a>
+              <span className="mx-2 text-xs">ou</span>
               <Link to="/register">
-                <a className="hover:underline text-xs">cadastre-se</a>
+                <span className="hover:underline text-xs">Cadastre-se</span>
               </Link>
-            </div>
-          </div>
-        </header>
-    )
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
