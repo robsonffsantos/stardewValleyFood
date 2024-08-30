b3e29a054b6339a90738db7b6a1d2f7543133f0a
Robson <robson.2501@hotmail.com>
2024-08-30 01:35:55 -0300

    feat: create Login context

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react'
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState<{ name: string, email: string } | null>(null)
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user')
+    if (storedUser) {
+      setUser(JSON.parse(storedUser))
+    }
+  }, [])
+
+  const login = (email, password) => {
+    const storedUser = localStorage.getItem('user')
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      if (parsedUser.email === email && parsedUser.password === password) {
+        setUser(parsedUser)
+        return true
+      }
+    }
+    return false
+  }
+
+  const logout = () => {
+    setUser(null)
+    localStorage.removeItem('user')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
