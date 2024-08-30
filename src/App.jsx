7c201422d723335fc60daee44c29de4835aa2e37
Robson <robson.2501@hotmail.com>
2024-08-30 02:23:09 -0300

    fix: turn profile a protexted route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import RestaurantDetails from './pages/RestaurantsDetails'
 import Profile from './pages/Profile'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
@@ -23,7 +24,7 @@ const App = () => {
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path="/restaurant/:id" element={<RestaurantDetails />} />
-          <Route path="/profile" element={<Profile />} />
+          <ProtectedRoute path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
